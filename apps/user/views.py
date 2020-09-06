960845d45c6b87b27ccf7a6794496f7bd4fece8c
mmillares <manuelmillares1@gmail.com>
2020-09-06 00:04:07 -0400

    Added login API

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,9 +1,20 @@
 from rest_framework import viewsets
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 from apps.user import serializers
 from apps.core import models
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 
 class UserViewSet(viewsets.ModelViewSet):
     """Handle creating and updating profiles"""
+    authentication_classes = (TokenAuthentication,)
     serializer_class = serializers.UserSerializer
     queryset = models.User.objects.all()
+    permission_classes = (IsAuthenticated,)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication token"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
