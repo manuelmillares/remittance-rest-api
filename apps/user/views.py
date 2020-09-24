831c8e8bb7d60b1963c23c7d02252126f23c103a
mmillares <manuelmillares1@gmail.com>
2020-09-24 13:41:43 -0400

    Added custom error message on User login

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from apps.user import serializers
@@ -7,6 +7,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+from django.contrib.auth import get_user_model
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -24,12 +25,38 @@ class UserLoginApiView(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data,
                                            context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({
-            'token': token.key,
-            'user_id': user.id,
-            'email': user.email,
-            'role': user.get_role_display()
-        })
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({
+                'token': token.key,
+                'user_id': user.id,
+                'email': user.email,
+                'role': user.get_role_display()
+            })
+        else:
+            email = request.data.get('username')
+            password = request.data.get('password')
+
+            if email is None or password is None:
+                return Response({'success': False,
+                                 'message': 'Missing credentials',
+                                 'data': request.data},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+            # email exists in request, try to find user
+            try:
+                user = get_user_model().objects.get(
+                    email=email
+                )
+            except:
+                return Response({'success': False,
+                                 'message': 'User not found',
+                                 'data': request.data},
+                                status=status.HTTP_404_NOT_FOUND)
+
+            if not user.check_password(password):
+                return Response({'success': False,
+                                 'message': 'Incorrect password',
+                                 'data': request.data},
+                                status=status.HTTP_403_FORBIDDEN)
