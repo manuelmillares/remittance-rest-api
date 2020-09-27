9ddf25c3a1127fd2e155b0f01f833a29b51d3e05
mmillares <manuelmillares1@gmail.com>
2020-09-27 00:26:16 -0400

    Overrided Remittances get_queryset method

--- apps/remittance/views.py
+++ apps/remittance/views.py
@@ -7,7 +7,12 @@ from rest_framework.authentication import TokenAuthentication
 
 class RemittanceViewSet(viewsets.ModelViewSet):
     """Handle creating and updating remittances"""
-    # authentication_classes = (TokenAuthentication,)
+    authentication_classes = (TokenAuthentication,)
     serializer_class = serializers.RemittanceSerializer
     queryset = models.Remittance.objects.all()
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return models.Remittance.objects.all()
+        return models.Remittance.objects.filter(distributor=self.request.user)
